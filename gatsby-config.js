a536420225b0195524f2a22b007f6ebe8281149e
Oluwatobi <tobylonge@gmail.com>
2020-06-20 11:24:18 +0100

    purged tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,14 @@ module.exports = {
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: false,
+        develop: false,
+        tailwind: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
